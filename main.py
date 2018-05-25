310a2ae3df293fc9dd15a782c0a838cd05ca08c3
BastyZ <bastian.inostroza@gmail.com>
2018-05-25 13:22:10 -0400

    Ventana con fondo funcionando creación de Ventana "a mano"

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'  # Centra la ventana
 def main():
     ancho = 1280
     alto = 720
-    init(ancho, alto, "Tarea 2 C. Grafica")
+    screen = pygame.display.set_mode((ancho, alto))
+    # init(ancho, alto, "Tarea 2 C. Grafica")
     vista = Vista()
 
     # Elementos de la pantalla
@@ -22,8 +23,35 @@ def main():
     clock = pygame.time.Clock()
 
     # Main Loop
-    while True:
+    run = True
+    while run:
         clock.tick(30)  # FPS
+
+        # Eventos en el juego
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run = False
+
+            # Keydown events
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    print("1 rayo lanzado")
+                    # TODO lanzar rayo
+                    # verificar colisiones con arboles
+                if event.key == K_m:
+                    print("random rayos lanzados")
+                    # TODO lanzar random(2,10) rayos
+                if event.key == K_a:
+                    print("crear arbol")
+                    # TODO crear arbol
+                if event.key == K_q:
+                    run = False
+
+        #Fondo
+        screen.fill((52, 82, 100))
+
+        # vuelca lo dibujado en pantalla
+        pygame.display.flip()
 
 
 main()
