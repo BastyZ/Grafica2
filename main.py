b9e5d87a78728bab029684e63d8f3057d9fc13d7
BastyZ <bastian.inostroza@gmail.com>
2018-05-25 12:20:03 -0400

    lograda ventana vacía en blanco

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # librerias
 import os
 import pygame
-import CC3501Utils
+from CC3501Utils import *
+from vista import *
+from modelo import *
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'  # Centra la ventana
+
 
 def main():
     ancho = 1280
     alto = 720
     init(ancho, alto, "Tarea 2 C. Grafica")
+    vista = Vista()
 
     # Elementos de la pantalla
     land_objects = []
@@ -17,7 +21,9 @@ def main():
     # Reloj del Juego
     clock = pygame.time.Clock()
 
-
     # Main Loop
     while True:
         clock.tick(30)  # FPS
+
+
+main()
