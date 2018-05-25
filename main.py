18dc5c04f93b831cd5baaa8a5833c22c48039aea
BastyZ <bastian.inostroza@gmail.com>
2018-05-24 20:48:35 -0400

    Agregada vista, reloj y un main loop para el juego

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ import os
 import pygame
 import CC3501Utils
 
-pygame.init()
 os.environ['SDL_VIDEO_CENTERED'] = '1'  # Centra la ventana
+
+def main():
+    ancho = 1280
+    alto = 720
+    init(ancho, alto, "Tarea 2 C. Grafica")
+
+    # Elementos de la pantalla
+    land_objects = []
+    obj = []
+
+    # Reloj del Juego
+    clock = pygame.time.Clock()
+
+
+    # Main Loop
+    while True:
+        clock.tick(30)  # FPS
