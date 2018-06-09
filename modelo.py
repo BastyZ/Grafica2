78507ec1278d2866486d1a35eed8432f01a8850c
BastyZ <bastian.inostroza@gmail.com>
2018-06-09 18:28:35 -0400

    editado el concepto de montaña

--- modelo.py
+++ modelo.py
@@ -2,9 +2,10 @@ from CC3501Utils import *
 
 
 class Montana(Figura):
-    def __init__(self, pos=Vector(0, 0), rgb=(1.0, 1.0, 1.0)):
-        super().__init__(pos, rgb)
+    def __init__(self):
+        self.dots = self.generate()
 
-    def figura(self):
+    def generate(self):
         print("")
         # TODO montaña
+        return []
