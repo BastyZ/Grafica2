cbe1c451cef8629d8c2170d7f64b740b1d279eed
BastyZ <bastian.inostroza@gmail.com>
2018-06-09 18:48:44 -0400

    iniciada generación de montaña, falta metodo "fractal"

--- modelo.py
+++ modelo.py
@@ -1,11 +1,15 @@
 from CC3501Utils import *
 
 
-class Montana(Figura):
-    def __init__(self):
+class Montana:
+    def __init__(self, ancho, altura):
+        self.ancho = ancho
+        self.alto = altura
         self.dots = self.generate()
 
     def generate(self):
-        print("")
-        # TODO montaña
-        return []
+        dot_list = [0] * self.ancho
+        dot_list[0] = self.alto
+        dot_list[self.ancho-1] = self.alto
+        # TODO recursive generation
+        return dot_list
